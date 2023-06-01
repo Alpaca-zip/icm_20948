3966d9ac32a724d73654b90929d6eb35305abafc
mori <kimihiro.mori[gmail.com>
2023-06-01 01:52:48 -0700

    first commit of icm_20948 v2.0

--- README.md
+++ README.md
@@ -1,78 +1,62 @@
-# ROS driver for the ICM-20948 
-[![](https://img.shields.io/badge/ROS-Noetic-brightgreen.svg)](https://github.com/Alpaca-zip/icm_20948)
+# ICM_20948 v2.0 [![](https://img.shields.io/badge/ROS2-humble-important?style=flat-square&logo=ros)](https://github.com/Alpaca-zip/icm_20948/tree/humble-devel) [![](https://img.shields.io/badge/ROS-noetic-blue?style=flat-square&logo=ros)](https://github.com/Alpaca-zip/icm_20948/tree/noetic-devel)
+
+ROS package for the ICM-20948 with Seeeduino XIAO.  
+Older version: [old-devel](https://github.com/Alpaca-zip/icm_20948/tree/old-devel)
+
+<img src="https://user-images.githubusercontent.com/84959376/218235948-b36ffe70-4e4e-4186-acaf-ef2bd89b3470.png" width="320px"> 　　　　<img src="https://github.com/Alpaca-zip/icm_20948/assets/84959376/807b0fb9-970a-4a2f-97f3-b76d624296f2" width="400px">
+
 ## 1 Installation
-### 1.1 Install ROS package on PC 
+### 1.1 Install ROS package
 ```
-$ cd catkin_ws/src 
-
-$ git clone https://github.com/Alpaca-zip/icm_20948.git 
-
-$ cd .. && catkin_make
+$ cd ~/catkin_ws/src
+$ git clone -b noetic-devel https://github.com/Alpaca-zip/icm_20948.git 
+$ cd ~/catkin_ws
+$ rosdep install -r -y -i --from-paths .
+$ catkin build
 ```
-### 1.2 Download Arduino IDE on PC 
-Get the latest version from the download page.
 
-https://www.arduino.cc/en/software
-## 2 For Seeeduino Xiao 
-### 2.1 ICM-20948 interfacing with Seeeduino Xiao 
-<img src="https://user-images.githubusercontent.com/84959376/218235948-b36ffe70-4e4e-4186-acaf-ef2bd89b3470.png" width="320px">
+### 1.2 Download Arduino IDE
+Get the latest version from the [download page](https://www.arduino.cc/en/software).
 
-### 2.2 Preferences 
-After Arduino IDE is run, click File -> Preferences in the top menu of the IDE. When the Preferences window appears, copy and paste following link to the Additional Boards Manager URLs textbox. 
+## 2 For Seeeduino XIAO
+### 2.1 Interfacing with Seeeduino XIAO
+|  ICM-20948  |  Seeeduino XIAO  |
+| :----------------: | :------------: |
+| GND | GND |
+| 3V3 | VIN |
+| DA  | SDA |
+| CL  | SCL |
 
-https://files.seeedstudio.com/arduino/package_seeeduino_boards_index.json 
+### 2.2 Preferences
+After Arduino IDE is run, click `File -> Preferences` in the top menu of the IDE. When the Preferences window appears, copy and paste following link to the Additional Boards Manager URLs textbox.
 
-### 2.3 Install the board package via Boards Manager 
-Click Tools -> Board -> Boards Manager..., print keyword "Seeeduino XIAO" in the searching blank. Here comes the "Seeed SAMD Boards". Install it. 
+`https://files.seeedstudio.com/arduino/package_seeeduino_boards_index.json`
 
-### 2.4 Select your board and port 
-Click Tools -> Board, find "Seeeduino XIAO M0" and select it. Now you have already set up the board of Seeeduino XIAO for Arduino IDE. Then, Select the serial device of the Arduino board from the Tools | Serial Port menu. This is likely to be COM3 or higher.  
+### 2.3 Install the board package via Boards Manager
+Click `Tools -> Board -> Boards Manager`..., print keyword "Seeeduino XIAO" in the searching blank. Here comes the "Seeed SAMD Boards". Install it.
 
-### 2.5 Download the Arduino library for ICM-20948 and ROS 
-Click Sketch -> Include Library -> Manage Libraries... 
+### 2.4 Select your board and port
+Click `Tools -> Board`, find "Seeeduino XIAO M0" and select it. Now you have already set up the board of Seeeduino XIAO for Arduino IDE. Then, Select the serial device of the Arduino board from `Tools -> Port` menu. This is likely to be COM3 or higher.
 
-### 2.5.1 ICM-20948 library 
-Print keyword " ICM 20948" in the searching blank. Here comes the "SparkFun 9DoF IMU Breakout - ICM-20948". Install it. 
+### 2.5 Download the Arduino library for ICM-20948 and ROS
+Click `Sketch -> Include Library -> Manage Libraries`..., print keyword " ICM 20948" in the searching blank. Here comes the "SparkFun 9DoF IMU Breakout - ICM-20948". Install it.
 
-***Important note:**
+> ***Important note:**  
+By default the DMP functionality is disabled in the library as the DMP firmware takes up 14301 Bytes of program memory.  
+To use the DMP, you will need to:  
+> 1. Edit ICM_20948_C.h  
+> 2. Uncomment line 29: #define ICM_20948_USE_DMP  
+> 3. Save changes
+> 
+> If you are using Windows, you can find ICM_20948_C.h in:  
+Documents\Arduino\libraries\SparkFun_ICM-20948_ArduinoLibrary\src\util
 
-**By default the DMP functionality is disabled in the library as the DMP firmware takes up 14301 Bytes of program memory.**
+### 2.6 Upload the sketch
+Open the “firmware/firmware.ino”. Then, simply click the `Upload` button in the environment. Wait a few seconds and if the upload is successful, the message "Done uploading." will appear in the status bar.
 
-**To use the DMP, you will need to:**
+## 3 Usage
+To run the node with a different port, for example on /dev/ttyACM0, you must specify the “port” parameters on the command line:
 
-**1, Edit ICM_20948_C.h**
-
-**2, Uncomment line 29: #define ICM_20948_USE_DMP**
-
-**3, Save changes**
-
-**If you are using Windows, you can find ICM_20948_C.h in:**
-
-**Documents\Arduino\libraries\SparkFun_ICM-20948_ArduinoLibrary\src\util**
-
-### 2.5.2 ROS library 
-Print keyword " Rosserial " in the searching blank. Here comes the " Rosserial Arduino Library". Install it. 
-
-### 2.6 Upload the program 
-Open the “include/ICM_20948_ROS_Example/ICM_20948_ROS_Example.ino”. Then, simply click the "Upload" button in the environment. Wait a few seconds and if the upload is successful, the message "Done uploading." will appear in the status bar. 
-
-## 3 Usage 
-### Launch icm_20948_example node 
-To run the node with a different port, for example on /dev/ttyACM0, you must specify the “PORT_NAME” parameters on the command line: 
 ```
-$ roslaunch icm_20948 icm_20948_example.launch PORT_NAME:=/dev/ttyACM0
+$ roslaunch icm_20948 run.launch port:=/dev/ttyACM0 debug:=true
 ```
-If the node is successfully launched, the terminal will print below messages and RViz will be opened. 
-<img src="https://user-images.githubusercontent.com/84959376/218236009-1a6c6516-e736-4eda-9435-49ad39b2b2dc.png" width="600px">
-
-<img src="https://user-images.githubusercontent.com/84959376/218236019-cbb6aa03-f029-441e-b5d7-e7cca034c700.png" width="600px">
-
-If you want to see other IMU data, you will need to: 
-
-1, Edit icm_20948_example.cpp 
-
-2, Uncomment line 15~17 
-
-3, Save changes 
-
-4, catkin_make 
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,97 +1,253 @@
-#define USE_USBCON // seeeduino xiao model needs this flag to be defined
+/****************************************************************
+ * This script is based on Example10_DMP_FastMultipleSensors.ino in the ICM 20948 Arduino Library.
+ * 
+ * ** Important note: by default the DMP functionality is disabled in the library
+ * ** as the DMP firmware takes up 14301 Bytes of program memory.
+ * ** To use the DMP, you will need to:
+ * ** Edit ICM_20948_C.h
+ * ** Uncomment line 29: #define ICM_20948_USE_DMP
+ * ** Save changes
+ * ** If you are using Windows, you can find ICM_20948_C.h in:
+ * ** Documents\Arduino\libraries\SparkFun_ICM-20948_ArduinoLibrary\src\util
+ ***************************************************************/
+
+#define USE_USBCON // seeeduino xiao needs this flag to be defined
 #define SERIAL_PORT Serial
 #define WIRE_PORT Wire
 #define AD0_VAL 1
-#define PI 3.1415926535
-#define MG 0.00980665
+#include "ICM_20948.h" // Click here to get the library: http://librarymanager/All#SparkFun_ICM_20948_IMU
 
-#include <ros.h>
-#include <sensor_msgs/Imu.h>
-#include <sensor_msgs/MagneticField.h>
-#include "ICM_20948.h"
+ICM_20948_I2C myICM; // Create an ICM_20948_I2C object
 
-ICM_20948_I2C myICM;
-
-ros::NodeHandle nh;
-
-sensor_msgs::Imu imu_msg;
-sensor_msgs::MagneticField mag_msg;
-ros::Publisher imu_pub("imu", &imu_msg);
-ros::Publisher mag_pub("mag", &mag_msg);
-
-void setup() {
+void setup()
+{
   SERIAL_PORT.begin(115200);
-  nh.getHardware()->setBaud(115200);
-  nh.initNode(); nh.getHardware()->setBaud(115200);
-  nh.advertise(imu_pub);
-  nh.advertise(mag_pub);
-  delay(100);
-
-  while (SERIAL_PORT.available())
-    SERIAL_PORT.read();
-  delay(1000);
-
+  while (!SERIAL_PORT);
   WIRE_PORT.begin();
   WIRE_PORT.setClock(400000);
-
   bool initialized = false;
-  while (!initialized) {
-    myICM.begin(WIRE_PORT, AD0_VAL);
-    if (myICM.status != ICM_20948_Stat_Ok) {
+  while (!initialized)
+  {
+    myICM.begin(WIRE_PORT, AD0_VAL); // Initialization of the sensor
+    if (myICM.status != ICM_20948_Stat_Ok)
+    {
       delay(500);
-    } else {
+    }
+    else
+    {
       initialized = true;
     }
   }
-
   bool success = true;
-  success &= (myICM.initializeDMP() == ICM_20948_Stat_Ok);
-  success &= (myICM.enableDMPSensor(INV_ICM20948_SENSOR_ORIENTATION) == ICM_20948_Stat_Ok);
-  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Quat9, 0) == ICM_20948_Stat_Ok);
-  success &= (myICM.enableDMPSensor(INV_ICM20948_SENSOR_MAGNETIC_FIELD_UNCALIBRATED) == ICM_20948_Stat_Ok);
-  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Cpass, 54) == ICM_20948_Stat_Ok);
-  success &= (myICM.enableFIFO() == ICM_20948_Stat_Ok);
-  success &= (myICM.enableDMP() == ICM_20948_Stat_Ok);
-  success &= (myICM.resetDMP() == ICM_20948_Stat_Ok);
-  success &= (myICM.resetFIFO() == ICM_20948_Stat_Ok);
+  success &= (myICM.initializeDMP() == ICM_20948_Stat_Ok); // Initialize the DMP
+  success &= (myICM.enableDMPSensor(INV_ICM20948_SENSOR_RAW_ACCELEROMETER) == ICM_20948_Stat_Ok); // 16-bit accel
+  success &= (myICM.enableDMPSensor(INV_ICM20948_SENSOR_RAW_GYROSCOPE) == ICM_20948_Stat_Ok); // 16-bit gyro + 32-bit calibrated gyro
+  success &= (myICM.enableDMPSensor(INV_ICM20948_SENSOR_GAME_ROTATION_VECTOR) == ICM_20948_Stat_Ok); // 32-bit 6-axis quaternion
+  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Quat6, 0) == ICM_20948_Stat_Ok); // Set to 225Hz
+  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Accel, 0) == ICM_20948_Stat_Ok); // Set to 225Hz
+  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Gyro, 0) == ICM_20948_Stat_Ok); // Set to 225Hz
+  success &= (myICM.enableFIFO() == ICM_20948_Stat_Ok); // Enable the FIFO
+  success &= (myICM.enableDMP() == ICM_20948_Stat_Ok); // Enable the DMP
+  success &= (myICM.resetDMP() == ICM_20948_Stat_Ok); // Reset DMP
+  success &= (myICM.resetFIFO() == ICM_20948_Stat_Ok); // Reset FIFO
+  if (!success)
+  {
+    while (1); // Do nothing more
+  }
 }
 
-void loop() {
+void loop()
+{
   icm_20948_DMP_data_t data;
   myICM.readDMPdataFromFIFO(&data);
-  myICM.getAGMT();
-  imu_msg.header.stamp = nh.now();
-  imu_msg.header.frame_id = "imu_link";
-  mag_msg.header.stamp = imu_msg.header.stamp;
-  mag_msg.header.frame_id = "mag_link";
-
-  if ((myICM.status == ICM_20948_Stat_Ok) || (myICM.status == ICM_20948_Stat_FIFOMoreDataAvail)) {
-    if ((data.header & DMP_header_bitmap_Quat9) > 0) {
-      double q1 = ((double)data.Quat9.Data.Q1) / 1073741824.0;
-      double q2 = ((double)data.Quat9.Data.Q2) / 1073741824.0;
-      double q3 = ((double)data.Quat9.Data.Q3) / 1073741824.0;
-      double q0 = sqrt(1.0 - ((q1 * q1) + (q2 * q2) + (q3 * q3)));
-      imu_msg.orientation.w = q0;
-      imu_msg.orientation.x = q1;
-      imu_msg.orientation.y = q2;
-      imu_msg.orientation.z = q3;
+  if ((myICM.status == ICM_20948_Stat_Ok) || (myICM.status == ICM_20948_Stat_FIFOMoreDataAvail))
+  {
+    if ((data.header & DMP_header_bitmap_Accel) > 0) // Check for Accel
+    {
+      int16_t raw_acc[4] = {data.Raw_Accel.Data.X, data.Raw_Accel.Data.Y, data.Raw_Accel.Data.Z, 0};
+      send_packet(0x51, raw_acc); // Send raw Acc values
     }
-    imu_msg.linear_acceleration.x = myICM.accX() * MG;
-    imu_msg.linear_acceleration.y = myICM.accY() * MG;
-    imu_msg.linear_acceleration.z = myICM.accZ() * MG;
-
-    imu_msg.angular_velocity.x = myICM.gyrX() * (PI / 180);
-    imu_msg.angular_velocity.y = myICM.gyrY() * (PI / 180);
-    imu_msg.angular_velocity.z = myICM.gyrZ() * (PI / 180);
-
-
-    if ((data.header & DMP_header_bitmap_Compass) > 0) {
-      mag_msg.magnetic_field.x = (float)data.Compass.Data.X;
-      mag_msg.magnetic_field.y = (float)data.Compass.Data.Y;
-      mag_msg.magnetic_field.z = (float)data.Compass.Data.Z;
-      mag_pub.publish(&mag_msg);
+    if ( (data.header & DMP_header_bitmap_Gyro) > 0 ) // Check for Gyro
+    {
+      int16_t raw_gyr[4] = {data.Raw_Gyro.Data.X, data.Raw_Gyro.Data.Y, data.Raw_Gyro.Data.Z, 0};
+      send_packet(0x52, raw_gyr); // Send raw Gyr values
+    }
+    if ((data.header & DMP_header_bitmap_Quat6) > 0) // Check for GRV data (Quat6)
+    {
+      // Debug feature
+      // The quaternion data is scaled by 2^30
+      // Scale to +/- 1
+      /*double q1 = ((double)data.Quat6.Data.Q1) / 1073741824.0; // Convert to double. Divide by 2^30
+      double q2 = ((double)data.Quat6.Data.Q2) / 1073741824.0;
+      double q3 = ((double)data.Quat6.Data.Q3) / 1073741824.0;
+      SERIAL_PORT.print(F("Q1:"));
+      SERIAL_PORT.print(q1, 3);
+      SERIAL_PORT.print(F(" Q2:"));
+      SERIAL_PORT.print(q2, 3);
+      SERIAL_PORT.print(F(" Q3:"));
+      SERIAL_PORT.println(q3, 3);*/
+      double q1 = ((double)data.Quat6.Data.Q1) / 1073741824.0; // Convert to double. Divide by 2^30
+      double q2 = ((double)data.Quat6.Data.Q2) / 1073741824.0;
+      double q3 = ((double)data.Quat6.Data.Q3) / 1073741824.0;
+      double q0 = sqrt(1.0 - ((q1 * q1) + (q2 * q2) + (q3 * q3)));
+      int16_t q1_scaled = (int16_t) (q1 * 32767.0);
+      int16_t q2_scaled = (int16_t) (q2 * 32767.0);
+      int16_t q3_scaled = (int16_t) (q3 * 32767.0);
+      int16_t q0_scaled = (int16_t) (q0 * 32767.0);
+      int16_t scaled_quat[4] = {q1_scaled, q2_scaled, q3_scaled, q0_scaled};
+      send_packet(0x53, scaled_quat); // Send scaled Quat values
     }
   }
-  imu_pub.publish(&imu_msg);
-  nh.spinOnce();
+  if (myICM.status != ICM_20948_Stat_FIFOMoreDataAvail) // If more data is available then we should read it right away - and not delay
+  {
+    delay(1);
+  }
+}
+
+void send_packet(byte packet_type, int16_t *data)
+{
+  byte checksum = 0x55 + packet_type;
+  Serial.write(0x55);
+  Serial.write(packet_type);
+  for (int i = 0; i < 4; i++)
+  {
+    byte low_byte = data[i] & 0xff;
+    byte high_byte = (data[i] >> 8) & 0xff;
+    Serial.write(low_byte);
+    Serial.write(high_byte);
+    checksum += low_byte + high_byte;
+  }
+  Serial.write(checksum);
+}
+
+// Overwrite initializeDMP function
+// Default initializeDMP function: https://github.com/sparkfun/SparkFun_ICM-20948_ArduinoLibrary/blob/f0b90f490762e705441373b6451785e2f9f84a11/src/ICM_20948.cpp#L1433-L1680
+ICM_20948_Status_e ICM_20948::initializeDMP(void)
+{
+  ICM_20948_Status_e  result = ICM_20948_Stat_Ok;
+  ICM_20948_Status_e  worstResult = ICM_20948_Stat_Ok;
+  result = i2cControllerConfigurePeripheral(0, MAG_AK09916_I2C_ADDR, AK09916_REG_RSV2, 10, true, true, false, true, true); if (result > worstResult) worstResult = result;
+  result = i2cControllerConfigurePeripheral(1, MAG_AK09916_I2C_ADDR, AK09916_REG_CNTL2, 1, false, true, false, false, false, AK09916_mode_single); if (result > worstResult) worstResult = result;
+  result = setBank(3); if (result > worstResult) worstResult = result;
+  uint8_t mstODRconfig = 0x04;
+  result = write(AGB3_REG_I2C_MST_ODR_CONFIG, &mstODRconfig, 1); if (result > worstResult) worstResult = result;
+  result = setClockSource(ICM_20948_Clock_Auto); if (result > worstResult) worstResult = result;
+  result = setBank(0); if (result > worstResult) worstResult = result;
+  uint8_t pwrMgmt2 = 0x40;
+  result = write(AGB0_REG_PWR_MGMT_2, &pwrMgmt2, 1); if (result > worstResult) worstResult = result;
+  result = setSampleMode(ICM_20948_Internal_Mst, ICM_20948_Sample_Mode_Cycled); if (result > worstResult) worstResult = result;
+  result = enableFIFO(false); if (result > worstResult) worstResult = result;
+  result = enableDMP(false); if (result > worstResult) worstResult = result;
+
+  //***************************************************************
+  ICM_20948_fss_t myFSS; // This uses a "Full Scale Settings" structure that can contain values for all configurable sensors
+  myFSS.a = gpm4; // gpm2, gpm4, gpm8, gpm16
+  myFSS.g = dps2000; // dps250, dps500, dps1000, dps2000
+  result = setFullScale((ICM_20948_Internal_Acc | ICM_20948_Internal_Gyr), myFSS); if (result > worstResult) worstResult = result;
+  //***************************************************************
+
+  result = enableDLPF(ICM_20948_Internal_Gyr, true); if (result > worstResult) worstResult = result;
+  result = setBank(0); if (result > worstResult) worstResult = result;
+  uint8_t zero = 0;
+  result = write(AGB0_REG_FIFO_EN_1, &zero, 1); if (result > worstResult) worstResult = result;
+  result = write(AGB0_REG_FIFO_EN_2, &zero, 1); if (result > worstResult) worstResult = result;
+  result = intEnableRawDataReady(false); if (result > worstResult) worstResult = result;
+  result = resetFIFO(); if (result > worstResult) worstResult = result;
+
+  //***************************************************************
+  ICM_20948_smplrt_t mySmplrt;
+  //mySmplrt.g = 19; // ODR is computed as follows: 1.1 kHz/(1+GYRO_SMPLRT_DIV[7:0]). 19 = 55Hz. InvenSense Nucleo example uses 19 (0x13).
+  //mySmplrt.a = 19; // ODR is computed as follows: 1.125 kHz/(1+ACCEL_SMPLRT_DIV[11:0]). 19 = 56.25Hz. InvenSense Nucleo example uses 19 (0x13).
+  mySmplrt.g = 4; // 225Hz
+  mySmplrt.a = 4; // 225Hz
+  //mySmplrt.g = 8; // 112Hz
+  //mySmplrt.a = 8; // 112Hz
+  result = setSampleRate((ICM_20948_Internal_Acc | ICM_20948_Internal_Gyr), mySmplrt); if (result > worstResult) worstResult = result;
+  //***************************************************************
+
+  result = setDMPstartAddress(); if (result > worstResult) worstResult = result;
+  result = loadDMPFirmware(); if (result > worstResult) worstResult = result;
+  result = setDMPstartAddress(); if (result > worstResult) worstResult = result;
+  result = setBank(0); if (result > worstResult) worstResult = result;
+  uint8_t fix = 0x48;
+  result = write(AGB0_REG_HW_FIX_DISABLE, &fix, 1); if (result > worstResult) worstResult = result;
+  result = setBank(0); if (result > worstResult) worstResult = result;
+  uint8_t fifoPrio = 0xE4;
+  result = write(AGB0_REG_SINGLE_FIFO_PRIORITY_SEL, &fifoPrio, 1); if (result > worstResult) worstResult = result;
+
+  //***************************************************************
+  // Configure Accel scaling to DMP
+  // The DMP scales accel raw data internally to align 1g as 2^25
+  // In order to align internal accel raw data 2^25 = 1g write 0x04000000 when FSR is 4g, 0x08000000 for 16g
+  const unsigned char accScale[4] = {0x04, 0x00, 0x00, 0x00};
+  result = writeDMPmems(ACC_SCALE, 4, &accScale[0]); if (result > worstResult) worstResult = result; // Write accScale to ACC_SCALE DMP register
+  // In order to output hardware unit data as configured FSR write 0x00040000 when FSR is 4g, 0x08000000 for 16g
+  const unsigned char accScale2[4] = {0x00, 0x04, 0x00, 0x00};
+  result = writeDMPmems(ACC_SCALE2, 4, &accScale2[0]); if (result > worstResult) worstResult = result; // Write accScale2 to ACC_SCALE2 DMP register
+  //***************************************************************
+
+  const unsigned char mountMultiplierZero[4] = {0x00, 0x00, 0x00, 0x00};
+  const unsigned char mountMultiplierPlus[4] = {0x09, 0x99, 0x99, 0x99};  // Value taken from InvenSense Nucleo example
+  const unsigned char mountMultiplierMinus[4] = {0xF6, 0x66, 0x66, 0x67}; // Value taken from InvenSense Nucleo example
+  result = writeDMPmems(CPASS_MTX_00, 4, &mountMultiplierPlus[0]); if (result > worstResult) worstResult = result;
+  result = writeDMPmems(CPASS_MTX_01, 4, &mountMultiplierZero[0]); if (result > worstResult) worstResult = result;
+  result = writeDMPmems(CPASS_MTX_02, 4, &mountMultiplierZero[0]); if (result > worstResult) worstResult = result;
+  result = writeDMPmems(CPASS_MTX_10, 4, &mountMultiplierZero[0]); if (result > worstResult) worstResult = result;
+  result = writeDMPmems(CPASS_MTX_11, 4, &mountMultiplierMinus[0]); if (result > worstResult) worstResult = result;
+  result = writeDMPmems(CPASS_MTX_12, 4, &mountMultiplierZero[0]); if (result > worstResult) worstResult = result;
+  result = writeDMPmems(CPASS_MTX_20, 4, &mountMultiplierZero[0]); if (result > worstResult) worstResult = result;
+  result = writeDMPmems(CPASS_MTX_21, 4, &mountMultiplierZero[0]); if (result > worstResult) worstResult = result;
+  result = writeDMPmems(CPASS_MTX_22, 4, &mountMultiplierMinus[0]); if (result > worstResult) worstResult = result;
+  const unsigned char b2sMountMultiplierZero[4] = {0x00, 0x00, 0x00, 0x00};
+  const unsigned char b2sMountMultiplierPlus[4] = {0x40, 0x00, 0x00, 0x00}; // Value taken from InvenSense Nucleo example
+  result = writeDMPmems(B2S_MTX_00, 4, &b2sMountMultiplierPlus[0]); if (result > worstResult) worstResult = result;
+  result = writeDMPmems(B2S_MTX_01, 4, &b2sMountMultiplierZero[0]); if (result > worstResult) worstResult = result;
+  result = writeDMPmems(B2S_MTX_02, 4, &b2sMountMultiplierZero[0]); if (result > worstResult) worstResult = result;
+  result = writeDMPmems(B2S_MTX_10, 4, &b2sMountMultiplierZero[0]); if (result > worstResult) worstResult = result;
+  result = writeDMPmems(B2S_MTX_11, 4, &b2sMountMultiplierPlus[0]); if (result > worstResult) worstResult = result;
+  result = writeDMPmems(B2S_MTX_12, 4, &b2sMountMultiplierZero[0]); if (result > worstResult) worstResult = result;
+  result = writeDMPmems(B2S_MTX_20, 4, &b2sMountMultiplierZero[0]); if (result > worstResult) worstResult = result;
+  result = writeDMPmems(B2S_MTX_21, 4, &b2sMountMultiplierZero[0]); if (result > worstResult) worstResult = result;
+  result = writeDMPmems(B2S_MTX_22, 4, &b2sMountMultiplierPlus[0]); if (result > worstResult) worstResult = result;
+
+  //***************************************************************
+  // Configure the DMP Gyro Scaling Factor
+  // @param[in] gyro_div Value written to GYRO_SMPLRT_DIV register, where
+  //            0=1125Hz sample rate, 1=562.5Hz sample rate, ... 4=225Hz sample rate, ...
+  //            10=102.2727Hz sample rate, ... etc.
+  // @param[in] gyro_level 0=250 dps, 1=500 dps, 2=1000 dps, 3=2000 dps
+  result = setGyroSF(4, 3); if (result > worstResult) worstResult = result; // 4 = 225Hz (see above), 3 = 2000dps (see above)
+
+  // Configure the Gyro full scale
+  // 2000dps : 2^28
+  // 1000dps : 2^27
+  //  500dps : 2^26
+  //  250dps : 2^25
+  const unsigned char gyroFullScale[4] = {0x10, 0x00, 0x00, 0x00}; // 2000dps : 2^28
+  result = writeDMPmems(GYRO_FULLSCALE, 4, &gyroFullScale[0]); if (result > worstResult) worstResult = result;
+
+  // Configure the Accel Only Gain: 15252014 (225Hz) 30504029 (112Hz) 61117001 (56Hz)
+  //const unsigned char accelOnlyGain[4] = {0x03, 0xA4, 0x92, 0x49}; // 56Hz
+  const unsigned char accelOnlyGain[4] = {0x00, 0xE8, 0xBA, 0x2E}; // 225Hz
+  //const unsigned char accelOnlyGain[4] = {0x01, 0xD1, 0x74, 0x5D}; // 112Hz
+  result = writeDMPmems(ACCEL_ONLY_GAIN, 4, &accelOnlyGain[0]); if (result > worstResult) worstResult = result;
+
+  // Configure the Accel Alpha Var: 1026019965 (225Hz) 977872018 (112Hz) 882002213 (56Hz)
+  //const unsigned char accelAlphaVar[4] = {0x34, 0x92, 0x49, 0x25}; // 56Hz
+  const unsigned char accelAlphaVar[4] = {0x3D, 0x27, 0xD2, 0x7D}; // 225Hz
+  //const unsigned char accelAlphaVar[4] = {0x3A, 0x49, 0x24, 0x92}; // 112Hz
+  result = writeDMPmems(ACCEL_ALPHA_VAR, 4, &accelAlphaVar[0]); if (result > worstResult) worstResult = result;
+
+  // Configure the Accel A Var: 47721859 (225Hz) 95869806 (112Hz) 191739611 (56Hz)
+  //const unsigned char accelAVar[4] = {0x0B, 0x6D, 0xB6, 0xDB}; // 56Hz
+  const unsigned char accelAVar[4] = {0x02, 0xD8, 0x2D, 0x83}; // 225Hz
+  //const unsigned char accelAVar[4] = {0x05, 0xB6, 0xDB, 0x6E}; // 112Hz
+  result = writeDMPmems(ACCEL_A_VAR, 4, &accelAVar[0]); if (result > worstResult) worstResult = result;
+  //***************************************************************
+
+  const unsigned char accelCalRate[4] = {0x00, 0x00};
+  result = writeDMPmems(ACCEL_CAL_RATE, 2, &accelCalRate[0]); if (result > worstResult) worstResult = result;
+  const unsigned char compassRate[2] = {0x00, 0x45};
+  result = writeDMPmems(CPASS_TIME_BUFFER, 2, &compassRate[0]); if (result > worstResult) worstResult = result;
+  return worstResult;
 }
